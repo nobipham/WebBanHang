cadaaae292e40525b468b19baeb21ff508bf2b48
nobipham <nobipham@users.noreply.github.com>
2020-12-30 02:22:03 +0700

    Update

--- js/web-trangchu.js
+++ js/web-trangchu.js
@@ -1,21 +1,3 @@
-$(document).ready(function() {
-    $(".menu-icon").on("click", function() {
-          $("nav ul").toggleClass("showing");
-    });
-});
-
-// Scrolling Effect
-
-$(window).on("scroll", function() {
-    if($(window).scrollTop()) {
-          $('nav').addClass('black');
-    }
-
-    else {
-          $('nav').removeClass('black');
-    }
-})
-
 jQuery(document).ready(function($){
   
     window.onload = function (){
